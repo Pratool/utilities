19f65ee556cfbdf3570e6bcfd35686dd858c9a5c
Pratool Gadtaula <5598110+Pratool@users.noreply.github.com>
2020-04-05 17:06:26 -0400

    feat: Add macros file

---
+++ macros.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Macros and functions
+
+# Quick dir switcher
+function workon()
+{
+    local PREFIX="${HOME}/Documents/projects"
+    local WORKSPACE="${1:-}"
+    if [[ -z "${WORKSPACE}" ]] || [[ ! -d "${PREFIX}/${WORKSPACE}" ]] ; then
+        echo "Invalid workspace: ${WORKSPACE}" >&2
+    else
+        cd "${PREFIX}/${WORKSPACE}"
+    fi
+}
